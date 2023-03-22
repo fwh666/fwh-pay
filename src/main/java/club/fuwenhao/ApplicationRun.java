b16325f4a472438e61ed2aaeba86215c4283ee35
ext.fuwenhao3 <ext.fuwenhao3@jd.com>
2023-03-22 18:58:58 +0800

    add:支付demo

--- src/main/java/club/fuwenhao/ApplicationRun.java
+++ src/main/java/club/fuwenhao/ApplicationRun.java
@@ -2,7 +2,11 @@ package club.fuwenhao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author fuwenhao
+ * @createDate 2023/3/22 17:50
+ * @descripton
+ */
 @SpringBootApplication
 public class ApplicationRun {
     public static void main(String[] args) {
---
+++ src/main/java/club/fuwenhao/config/ApiException.java
@@ -0,0 +1,26 @@
+package club.fuwenhao.config;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	private RetEnum ret;
+    private String retMessage;
+
+    public ApiException(RetEnum ret) {
+        super(ret.asMessage());
+        this.ret = ret;
+        this.retMessage = ret.asMessage();
+    }
+
+    public ApiException(RetEnum ret, String retMessage) {
+        super(retMessage);
+        this.ret = ret;
+        this.retMessage = retMessage;
+    }
+
+}
